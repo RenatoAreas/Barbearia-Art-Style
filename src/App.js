146a9c57cfa8661164855a179dce7f3ff28ea12f
Renato <rsareas93@gmail.com>
2021-04-21 22:31:49 -0300

    Tirando os comentarios do código

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-//bilbioteca padrão REACT
 import React from 'react';
 import Header from './components/Header';
 import Profile from './components/Profile';
 import Main from './views/Main';
 import { Row, Col } from 'react-materialize';
  
-//declarando o componente como uma classe
 class App extends React.Component {
  
-  //função render -> tem como objetivo retornar o conteudo
-  //html que o componente irá renderizar na página
   render() {
     return (
       <Row>
@@ -28,5 +24,4 @@ class App extends React.Component {
  
 }
  
-//tornando o componente publico
 export default App;
--- src/views/Home.js
+++ src/views/Home.js
@@ -3,7 +3,6 @@ import { Row, Col } from 'react-materialize';
 import Profile from '../components/Profile';
 import Products from '../components/Products';
  
-//declarando um componente como função
 const Home = () => (
     <Products />
 )
